128b526a37756c95d587757cbeea394e4cc00783
hankyung <gksrud246@naver.com>
2024-03-18 19:12:13 +0900

    0318공부

--- workall/javawork/Naver0314JavaProject/src/day0315/Ex12_munje.java
+++ workall/javawork/Naver0314JavaProject/src/day0315/Ex12_munje.java
@@ -22,8 +22,8 @@ public class Ex12_munje {
 		System.out.println("점수 입력");
 		score=sc.nextInt();
 		
-		msg=score>=90?"참 잘했어요!!" : score >= 80? "수고했어요!" :
-			score >= 70? "조금 더 노력하세요!" : "재시험입니다!";
+		msg= (score>= 90) ? ("참 잘했어요!!") : ((score >= 80)? ("수고했어요!") :
+			((score >= 70)? ("조금 더 노력하세요!") : ("재시험입니다!")));
 			
 		System.out.println(score+ "=>" +msg);
 				
