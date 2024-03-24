82fa0a54a28faf8fd998c71bc707f4c01756d2ec
hankyung <gksrud246@naver.com>
2024-03-24 17:42:02 +0900

    0324 공부

---
+++ workall/javawork/Naver0314JavaProject/src/day0324/TextRandom.java
@@ -0,0 +1,18 @@
+package day0324;
+
+public class TextRandom {
+	public static void main(String[] args) {
+		int month =(int)(Math.random()*12 + 1);
+
+		Random.test(month);
+
+
+
+
+	}
+
+
+
+
+
+}
