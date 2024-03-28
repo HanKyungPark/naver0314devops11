f0dfa6caf3b3aa581158bf7e92244a4c2f2e6785
hankyung <gksrud246@naver.com>
2024-03-28 10:34:52 +0900

    0327 공부

---
+++ workall/javawork/Naver0314JavaProject/src/day0327/StudentDto.java
@@ -0,0 +1,88 @@
+package day0327;
+
+public class StudentDto {
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getJava() {
+		return java;
+	}
+	public void setJava(int java) {
+		this.java = java;
+	}
+	public int getSpring() {
+		return spring;
+	}
+	public void setSpring(int spring) {
+		this.spring = spring;
+	}
+	public int getHtml() {
+		return html;
+	}
+	public void setHtml(int html) {
+		this.html = html;
+	}
+	private String name;
+	private int age;
+	private int java;
+	private int spring;
+	private int html;
+	public StudentDto() {
+		super();
+	}
+	public StudentDto(String name, int age, int java, int spring, int html) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.java = java;
+		this.spring = spring;
+		this.html = html;
+	}
+	public int getTotal()
+	{
+		return java+spring+html;
+	}
+	
+	public double getAvg()
+	{
+		return getTotal()/3.0;
+	}
+	
+	public String getGrade()
+	{
+		if(getAvg()>=90)
+			return "*****";
+		else if(getAvg()>=80)
+			return "*****";
+		else if(getAvg()>=70)
+			return"***";
+		else
+			return"*";
+	}
+	
+	
+	
+	//디폴트 생성자
+	
+	//매개변수 있는 생성자
+	
+	//setter & getter
+	
+	
+	//여기서부터 직접 
+	//java+spring+html 의 합을 반환하는 메서드(getTotal)
+	
+	//getTotal()/3.0 으로 나눠서 반환하는 메서드 (getAvg)
+	
+	//getAvg() 의 값이 90 이상이면 "*****", 80이상이면 "*****", 70이상이면 "***" ,나머지는"*"
+	//반환하는 메서드 (getGrade)
+}
